309f621bf88250e150e6bde1a9c94ab163a0451b
Matt Craddock <5796417+craddm@users.noreply.github.com>
2024-10-29 13:19:27 +0000

    Add test of SRE teardown cancellation

--- tests/commands/test_sre.py
+++ tests/commands/test_sre.py
@@ -100,11 +100,9 @@ class TestDeploySRE:
 class TestTeardownSRE:
     def test_teardown(
         self,
-        mocker: MockerFixture,
         runner: CliRunner,
         mock_ip_1_2_3_4,  # noqa: ARG002
         mock_pulumi_config_from_remote,  # noqa: ARG002
-        mock_shm_config_from_remote,  # noqa: ARG002
         mock_sre_config_from_remote,  # noqa: ARG002
         mock_sre_project_manager_teardown_then_exit,  # noqa: ARG002
     ) -> None:
@@ -139,3 +137,15 @@ class TestTeardownSRE:
         assert result.exit_code == 1
         assert "mock get_credential\n" in result.stdout
         assert "mock get_credential error" in result.stdout
+
+    def test_teardown_cancelled(
+        self,
+        runner: CliRunner,
+        mock_ip_1_2_3_4,  # noqa: ARG002
+        mock_pulumi_config_from_remote,  # noqa: ARG002
+        mock_sre_config_from_remote,  # noqa: ARG002
+        mock_sre_project_manager_teardown_then_exit,  # noqa: ARG002
+    ) -> None:
+        result = runner.invoke(sre_command_group, ["teardown", "sandbox"], input="n")
+        assert result.exit_code == 0
+        assert "cancelled by user" in result.stdout
