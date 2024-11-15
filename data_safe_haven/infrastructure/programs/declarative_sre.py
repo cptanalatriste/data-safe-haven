52f86c170a2611beaf9116b2f0be35ebad38d970
Matt Craddock <5796417+craddm@users.noreply.github.com>
2024-11-15 16:28:58 +0000

    Export sre_fqdn

--- data_safe_haven/infrastructure/programs/declarative_sre.py
+++ data_safe_haven/infrastructure/programs/declarative_sre.py
@@ -420,4 +420,5 @@ class DeclarativeSRE:
         pulumi.export("data", data.exports)
         pulumi.export("ldap", ldap_group_names)
         pulumi.export("remote_desktop", remote_desktop.exports)
+        pulumi.export("sre_fqdn", networking.sre_fqdn)
         pulumi.export("workspaces", workspaces.exports)
