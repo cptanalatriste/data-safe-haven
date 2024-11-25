d5042b2cd262630f4c6a644c1707fd238e178436
Matt Craddock <5796417+craddm@users.noreply.github.com>
2024-11-25 22:11:17 +0000

    Export context name associated with SRE as part of SRE stack

--- data_safe_haven/infrastructure/programs/declarative_sre.py
+++ data_safe_haven/infrastructure/programs/declarative_sre.py
@@ -424,7 +424,7 @@ class DeclarativeSRE:
         # Export values for later use
         pulumi.export("data", data.exports)
         pulumi.export("ldap", ldap_group_names)
+        pulumi.export("context", self.context.name)
         pulumi.export("remote_desktop", remote_desktop.exports)
         pulumi.export("sre_fqdn", networking.sre_fqdn)
-        pulumi.export("linked_shm", self.context.name)
         pulumi.export("workspaces", workspaces.exports)
