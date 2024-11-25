07659ad436f79a43daacd842e54277ac71d5d524
Jim Madge <jmadge@turing.ac.uk>
2024-11-25 09:41:20 +0000

    Fix linting

--- data_safe_haven/infrastructure/components/wrapped/__init__.py
+++ data_safe_haven/infrastructure/components/wrapped/__init__.py
@@ -2,6 +2,6 @@ from .log_analytics_workspace import WrappedLogAnalyticsWorkspace
 from .nfsv3_storage_account import WrappedNFSV3StorageAccount
 
 __all__ = [
-    "WrappedNFSV3StorageAccount",
     "WrappedLogAnalyticsWorkspace",
+    "WrappedNFSV3StorageAccount",
 ]
